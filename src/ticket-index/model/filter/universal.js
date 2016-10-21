198a943d8ce5a8c6d1fcda7b186b33cf2e276bf9
YuuKohaku <soul.of.bytes@gmail.com>
2016-10-21 15:33:25 +0700

    1

--- src/ticket-index/model/filter/universal.js
+++ src/ticket-index/model/filter/universal.js
@@ -3,7 +3,7 @@
 let hasIntersection = require("../util/has-intersection.js");
 
 module.exports = function (prefiltered, source, params) {
-	console.log("univ", prefiltered);
+	// console.log("univ", prefiltered);
 	let l = prefiltered.length,
 		i = -1,
 		src;
@@ -12,26 +12,35 @@ module.exports = function (prefiltered, source, params) {
 		if (!prefiltered[i]) continue;
 		src = source[prefiltered[i]];
 
-		if (src.properties.operator && !hasIntersection(params.operator, src.properties.operator))
-			prefiltered[i] = false;
 
-		if (src.properties.booking_method == 'prebook' &&
-			src.properties.time_description[0] > (params.now + params.prebook_show_interval + 30)) {
-			// console.log(src.properties.time_description[0], params.now, (params.now + params.prebook_show_interval + 30));
+		if (src.properties.destination && !hasIntersection(params.destination, src.properties.destination)) {
 			prefiltered[i] = false;
+			continue;
+		}
+
+		if (src.properties.operator && !hasIntersection(params.operator, src.properties.operator)) {
+			prefiltered[i] = false;
+			continue;
+		}
+
+
+		if (params.state !== '*' && !~params.state.indexOf(src.properties.state)) {
+			prefiltered[i] = false;
+			continue;
+		}
+
+		if (params.service !== '*' && !~params.service.indexOf(src.properties.service)) {
+			prefiltered[i] = false;
+			continue;
 		}
 
 		if (src.properties.booking_method == 'prebook' &&
 			src.properties.time_description[0] > (params.now + params.prebook_show_interval + 30)) {
-			// console.log(src.properties.time_description[0], params.now, (params.now + params.prebook_show_interval + 30));
 			prefiltered[i] = false;
+			continue;
 		}
 
-		if (params.state !== '*' && !~params.state.indexOf(src.properties.state))
-			prefiltered[i] = false;
 
-		if (src.properties.destination && !hasIntersection(params.destination, src.properties.destination))
-			prefiltered[i] = false;
 	}
 	return prefiltered;
 };
